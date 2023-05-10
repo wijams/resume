a9609267b647a4cd475bcf9c98e641a52260b9c1
Wesley Ijams <wijams@byu.edu>
2023-05-10 14:34:30 -0600

    Add banner to landing page.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,23 @@
+import styled from '@emotion/styled';
 import Header from './Header'
+import picture from '../Images/Forest.jpeg'
+
+const Banner = styled.div`
+    overflow: hidden;
+    height: 24rem;
+`
+
+const BannerImage = styled.img`
+    width: 100%;
+`
 
 function Home() {
     return (
         <>
             <Header/>
-            <h1>Wesley Ijams</h1>
+            <Banner>
+                <BannerImage src={picture} alt="The website owner"/>
+            </Banner>
         </>
     )
 }
